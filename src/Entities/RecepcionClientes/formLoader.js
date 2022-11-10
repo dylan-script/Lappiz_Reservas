d5d56ca14ea813274f6e0d3b3cf9808e69c7988e
Dilan Jimenez Arroyave <Dilan.Jimenez@softgic.co>
2022-11-10 12:48:39 -0500

    Saving Data of the recepcion (Error)

--- src/Entities/RecepcionClientes/formLoader.js
+++ src/Entities/RecepcionClientes/formLoader.js
@@ -5,27 +5,37 @@ setTimeout(() => {
   var appViewId = getAppViewId();
   console.log(appViewId);
   if (appViewId == 'da587085-4398-4565-96e7-19114eba4a9b') {
-    alert('Bienvenid@s')
-    //
-    // Método encargado de dar visibilidad a una sección específica
-    // True para mostrar - False para ocultar
     var registerSection = '9015e84f-43a4-4f4c-844a-c1ce3679be59'
     var searchSection = '31acd8df-fe41-405e-94ad-f4e0c49f50b5'
-    visibilitySection(registerSection, true);
+    var campoId = 'c8cee7d9-4907-4c16-bf4a-566a300bb92f'
+    visibilitySection(registerSection, false);
     visibilitySection(searchSection, false);
+    if (e.isNew) {
+    alert('Bienvenid@s')
+
+      requireField(campoId, true);
 
 
 
-    $("#register-btn").click(function () {
-      debugger
+      $(".register").click(function () {
+        debugger
+        visibilitySection(registerSection, true);
+        visibilitySection(searchSection, false);
+        sessionStorage.Option = 0
+        console.log(sessionStorage.Option)
+      });
+
+      $(".search").click(function () {
+        debugger
+        visibilitySection(registerSection, false);
+        visibilitySection(searchSection, true);
+        sessionStorage.Option = 1
+        console.log(sessionStorage.Option)
+      });
+    }else {
+      var viewSection = '0977cfba-62cf-49cf-87eb-daa8f7286b10';
+      visibilitySection(viewSection, false);
       visibilitySection(registerSection, true);
-      visibilitySection(searchSection, false);
-    });
-
-    $("#search-btn").click(function () {
-      debugger
-      visibilitySection(registerSection, false);
-      visibilitySection(searchSection, true);
-    });
+    }
   }
-}, 1000);
+}, 3000);
